3e2c726d09363acd35750ab85e376b91319c5107
Bunker2420 <sairuthwickt@gmail.com>
2025-05-07 17:00:31 +0530

    Changed ui interface

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.use((req,res,next)=>{
 
 app.get('/',(req,res)=>{
     res.redirect('/listings')
-})
+});
 // useed express.router for user page
 const userroute = require('./routes/user.js');
 app.use('/SignUp',userroute);
